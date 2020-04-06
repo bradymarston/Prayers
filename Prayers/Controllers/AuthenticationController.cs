8070973d2793b22abc09eec9d50b3962e0c3e94e
Brady Marston <bradymarston@live.com>
2020-04-06 16:17:12 -0500

    Added AuthorizationController

---
+++ Prayers/Controllers/AuthenticationController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Prayers.Controllers.Dtos;
+using Prayers.Models;
+using ShadySoft.ControllerErrorHelpers.Extensions.Controller;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace Prayers.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthenticationController(SignInManager<ApplicationUser> signInManager,
+                                        UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginDto credentials)
+        {
+            var user = await _userManager.FindByNameAsync(credentials.UserName);
+
+            if (user is null)
+                return this.SignInFailure(SignInResult.Failed);
+
+            var result = await _signInManager.PasswordSignInAsync(user, credentials.Password, credentials.RememberMe, true);
+
+            if (!result.Succeeded)
+                return this.SignInFailure(result);
+
+            var principal = await _signInManager.CreateUserPrincipalAsync(user);
+
+            return Ok(GenerateReturnedUser(principal));
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Ok();
+        }
+
+        [HttpGet("User")]
+        public IActionResult GetCurrentUser()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return NotFound();
+
+            return Ok(GenerateReturnedUser(User));
+        }
+
+        private IEnumerable<IEnumerable<string>> GenerateReturnedUser(ClaimsPrincipal user)
+        {
+            return user.Claims.Select(c => new List<string> { c.Type, c.Value });
+        }
+    }
+}
---
+++ Prayers/Data/Migrations/20200406211241_SeedAdminUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prayers.data.Migrations
+{
+    public partial class SeedAdminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [dbo].[AspNetUsers] ([Id], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) VALUES (N'33bd8cd2-efc3-4136-848d-2e65e2340b60', N'Admin', N'ADMIN', NULL, NULL, 0, N'AQAAAAEAACcQAAAAEM4SeN6tdByABTSMI4TKjr/dvSjX/NLujT0KIATL+vsbOUpS3kuX/fRkKbuZlwkytg==', N'A6425R7L7OXYL3VFYUDBLIS6OV3PPODL', N'a70b4955-bf84-4b54-a032-506b2fc5717a', NULL, 0, 0, NULL, 1, 0)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("AspNetUsers", "Id", "33bd8cd2-efc3-4136-848d-2e65e2340b60");
+        }
+    }
+}
